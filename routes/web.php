2116ffe3ddee5a165d88bfc1a252f0abdc22ccff
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-03-24 21:48:51 +0100

    Première utilisation des controllers

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,17 @@
 |
 */
 
+//Routes classiques directement vers une Vue en particulier
+/* 
 Route::get('/', function () {
-    return view('welcome');
+     return view('welcome');
 });
-
 
 Route::get('article/{n}', function($n) { 
     return view('article')->with('numero', $n); 
 })->where('n', '[0-9]+');
+*/
+
+//Routes vers un Controlleur qui décide de quelle vue afficher 
+Route::get('/', 'WelcomeController@index');
+Route::get('article/{n}', 'ArticleController@show')->where('n', '[0-9]+');

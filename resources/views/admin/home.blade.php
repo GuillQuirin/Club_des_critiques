bcd97a0ac1c2cefc3a7b2891267792c4ac7db4a0
Elise <elisepoirier@outlook.com>
2017-04-12 23:04:12 +0200

    Intégration page admin - en cours

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,89 @@
+<div class="panel-body">
+	<div class="row">
+		<form class="col-md-12">
+			<div class="form-group">
+	    		<h4>Le concept : </h4>
+		    	<textarea class="form-control" id="contenu" rows="3"></textarea>
+			</div>
+			<button type="submit" class="btn btn-success pull-right">Modifier</button>
+		</form>
+	</div>	
+
+	<div class="row">
+		<div class="col-md-12">
+			<h4>A la une : </h4><br>
+
+			<p>
+	          <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseAddElementTop" aria-expanded="false" aria-controls="collapseAddElementTop">
+	            Ajouter une oeuvre
+	          </button>
+	        </p>
+	        <div class="collapse" id="collapseAddElementTop">
+	          	<div class="card card-block">
+	            	<form>
+	            	<!-- Select avec recherche auto completion -->
+	            		<div class="form-group">
+	                        <label for="top_parent_cat" class="col-2 col-form-label">Catégorie : </label>
+	                        <div class="col-10">
+	                            <select class="form-control" id="top_parent_cat" name="parent_cat">*
+                                	<option>Choisir une catégorie</option>	                           
+	                                <option value="1">Livre</option>
+	                                <option value="2">Film</option>
+	                                <option value="3">Expo</option>
+	                            </select>
+	                        </div>
+	                    </div>
+
+	                    <div class="form-group">
+	                        <label for="creator" class="col-2 col-form-label">Auteur / réalisateur : </label>
+	                        <div class="col-10">
+	                            <select class="form-control" id="creator" name="creator">
+	                                <option value="1">Victor Hugo</option>
+	                                <option value="2">Molière</option>
+	                                <option value="3">Emile Zola</option>
+	                            </select>
+	                        </div>
+	                    </div>
+
+	                    <div class="form-group">
+	                        <label for="creator" class="col-2 col-form-label">Titre : </label>
+	                        <div class="col-10">
+	                            <select class="form-control" id="creator" name="creator">
+	                                <option value="1">Le petit chaperon rouge</option>
+	                                <option value="2">Les misérables</option>
+	                                <option value="3">...</option>
+	                            </select>
+	                        </div>
+	                    </div>
+	                    
+	                    <button type="submit" class="btn btn-success pull-right">Ajouter</button>
+	            	</form>
+	            </div>
+	            <br>
+	        </div>
+
+			<table id="elementTopTable" class="display" cellspacing="0" width="100%">
+				<thead>
+					<tr>
+						<th>Nom</th>
+						<th>Auteur</th>
+						<th>Catégorie</th>
+						<th>Sous catégorie</th>
+						<th>Action</th>            					
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>Le petit chaperon rouge</th>
+						<th>Elise Poirier</th>
+						<th>Livre</th>
+						<th>Pour les bébé</th>
+						<th>
+							<i class="fa fa-trash" aria-hidden="true"></i>
+						</th>            					
+					</tr>
+				</tbody>		
+			</table>
+		</div>
+	</div>
+</div>
---
+++ resources/views/admin/user.blade.php
@@ -0,0 +1,162 @@
+<div id="collapse5" class="panel-collapse collapse">
+    <div class="panel-body">
+        <p>
+          <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseAddUser" aria-expanded="false" aria-controls="collapseAddUser">
+            Ajouter un utilisateur
+          </button>
+        </p>
+        <div class="collapse" id="collapseAddUser">
+            <div class="card card-block">
+                <form>
+                    <div class="form-group">
+                        <label for="last_name" class="col-2 col-form-label">Nom : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" id="last_name" name="last_name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="first_name" class="col-2 col-form-label">Prénom : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" id="first_name" name="first_name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="col-2 col-form-label">Description : </label>
+                        <div class="col-10">
+                            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="picture" class="col-2 col-form-label">Url image : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" id="picture" name="picture">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="location" class="col-2 col-form-label">Département : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="date" id="location" name="location">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="col-2 col-form-label">Email : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="date" id="email" name="email">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-success pull-right">Ajouter un utilisateur</button>
+                </form>
+                <br><br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <table id="userTable" class="display" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Prénom</th>
+                            <th>Département</th>
+                            <th>Url image</th>
+                            <th>Email</th>
+                            <th>Statut</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Poirier</th>
+                            <th>Elise</th>
+                            <th>Yvelines</th>
+                            <th>url</th>
+                            <th>elise@test.fr</th>
+                            <th>Valide</th>
+                            <th>
+                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                <i class="fa fa-pencil" data-toggle="modal" data-target="#editUserModal" aria-hidden="true"></i>
+                            </th>                               
+                        </tr>                               
+                        <tr>
+                            <th>Poirier</th>
+                            <th>Elise</th>
+                            <th>Yvelines</th>
+                            <th>url</th>
+                            <th>elise@test.fr</th>
+                            <th>Valide</th>
+                            <th>
+                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                <i class="fa fa-pencil" data-toggle="modal" data-target="#editUserModal" aria-hidden="true"></i>
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<!-- MODAL MODIFICATION UTILISATEUR-->
+<div class="modal fade" id="editUserModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modifier l'utilisateur</h4>
+            </div>
+            <form>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="last_name" class="col-2 col-form-label">Nom : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" id="last_name" name="last_name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="first_name" class="col-2 col-form-label">Prénom : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" id="first_name" name="first_name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="col-2 col-form-label">Description : </label>
+                        <div class="col-10">
+                            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="picture" class="col-2 col-form-label">Url image : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="text" id="picture" name="picture">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="location" class="col-2 col-form-label">Département : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="date" id="location" name="location">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="col-2 col-form-label">Email : </label>
+                        <div class="col-10">
+                            <input class="form-control" type="date" id="email" name="email">
+                        </div>
+                    </div>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input type="checkbox" class="form-check-input" name="is_reported" id="is_reported">
+                            Bannir
+                        </label>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success">Modifier</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>

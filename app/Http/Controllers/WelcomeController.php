a73e297d7897a181ef385308ee8b5a041ae3e634
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-04-16 15:36:03 +0200

    Rassemblement des infos de l'Accueil dans WelcomeController

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -18,6 +18,24 @@ class WelcomeController extends Controller
 		 * get oeuvres à la une
 		 * get contact info
 		 */
-		return view('welcome');
+		$array = [
+			'url_background' => 'images/welcome.jpeg',
+			'title' => "Le club des critiques",
+			'slogan' => "Lisez, rencontrez, partagez",
+			'description' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		];
+
+		$popUp = 'element.show';
+		$array['items'] = [
+            ['id' => 1, 'name' => 'Harry Potter', 'subname' => 'Livre', 'url_img' => '/images/oeuvre.jpg', 'description' => "COUCOU"],
+            ['id' => 2, 'name' => 'Interstellar', 'subname' => 'Film', 'url_img' => '/images/oeuvre1.jpg', 'description' => "COUCOU"],
+            ['id' => 3, 'name' => 'Paris Games Week', 'subname' => 'Exposition', 'url_img' => '/images/oeuvre2.jpg', 'description' => "COUCOU"],
+     	];
+
+		return view('welcome')
+				->with(compact('array'))
+				->with(compact('popUp'));
 	}
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,59 +1,32 @@
 @extends('templates/template')
 
 @section('css')
-     <link rel="stylesheet" type="text/css" href="{{asset('css/welcome.css')}}">    
+     <link rel="stylesheet" type="text/css" href="{{asset('css/welcome.css')}}">  
+     <style type="text/css">
+         .jumbotron{background-image: url({{$array['url_background']}});}
+     </style>  
 @endsection
 
 @section('title')
-    Club des critiques
+    {{$array['title']}}
 @endsection
 
 @section('content')
     
     <div class="jumbotron">
       <div class="container-fluid">
-        <h1>Le club des critiques <br><small>Lisez, rencontrez, partagez</small></h1>
+        <h1>{{$array['title']}} <br><small>{{$array['slogan']}}</small></h1>
       </div>
     </div>
     <div class="container notPadding">       
         <div>
             <h2>Le concept</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <p>{{$array['description']}}</p>
         </div>
     
         <hr>
 
-        <div class="row">
-            <h2>A la une</h2>
-            <?php for($i=0; $i<6; $i++): ?>
-                <div class="col-xs-6 col-md-4">
-                    <a href="#" class="thumbnail">
-                        <figure>
-                            <img src="/images/oeuvre.jpg" alt="...">
-                            <figcaption>
-                                <p class="title">Titre</p>
-                                <p class="author">Auteur</p>
-                            </figcaption>
-                        </figure>
-                    </a>
-                </div>
-            <?php endfor; ?>
-        </div>
+        @include('templates.mosaique')
         
         <hr>
 

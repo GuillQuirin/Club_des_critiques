fe6f8231ec891b18c2e5d4df0712b2bc7994fbef
Laurie <l.guibert@hotmail.fr>
2017-04-21 11:58:08 +0200

    Création de la modal rejoindre le salon

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -28,11 +28,48 @@
                     <td>Harry Potter <?php echo $i;?> (J.K Rowling)</td>
                     <td>Du 01/05/2017 au 01/07/2017</td>
                     <td>Salon 1</td>
-                    <td><button type="button" class="btn btn-success">Rejoindre le salon</button></td>
+                    <td><button type="button"
+                                class="btn btn-success"
+                                data-toggle="modal"
+                                data-target="#join"
+                                data-title="Harry Potter <?php echo $i?>"
+                                data-autor="J.K Rowling"
+                                data-salon="Salon 1">
+                            Rejoindre le salon
+                        </button>
+                    </td>
                 </tr>
                 <?php } ?>
             </tbody>
         </table>
+    </div>
+
+    <div class="modal fade" id="join" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <h1 class="text-center text-uppercase col-xs-10 col-sm-12" id="title"></h1>
+                    <h1 class="text-center text-uppercase col-xs-10 col-sm-12" id="autor"><small></small></h1>
+
+                    <div class="text-center" id="div_note">
+                        <h3>Donnez une note !</h3>
+                        <div class="rating">
+                            <a href="#5" title="Donner 5 étoiles">☆</a>
+                            <a href="#4" title="Donner 4 étoiles">☆</a>
+                            <a href="#3" title="Donner 3 étoiles">☆</a>
+                            <a href="#2" title="Donner 2 étoiles">☆</a>
+                            <a href="#1" title="Donner 1 étoile">☆</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Rejoindre</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -51,6 +88,17 @@
                     "lengthMenu": "Affichage par _MENU_ enregistrements"
                 }
             });
-        } );
+
+            $('#join').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget)
+                var title = button.data('title')
+                var autor = button.data('autor')
+                var salon = button.data('salon')
+                var modal = $(this)
+                modal.find('.modal-body #title').text(title + " - " + salon)
+                modal.find('.modal-body #autor small').text(autor)
+                modal.find('.modal-title').text(salon)
+            })
+        });
     </script>
 @endsection

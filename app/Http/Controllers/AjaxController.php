239a7c747cfe877aeffc809625ed7ce782efd22d
Elise <elisepoirier@outlook.com>
2017-06-26 18:35:23 +0200

    Admin - gestion des oeuvres suggérées

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -8,6 +8,7 @@ use App\Room;
 use App\Element;
 use App\Category;
 use App\UserRoom;
+use App\ElementSuggest;
 use App\Report;
 
 use Illuminate\Http\Request;
@@ -241,5 +242,35 @@ class AjaxController extends Controller
         $report->save();
 
         return Response::json($user_room);
+    }
+
+    /**
+     * Ajax Request : validate an element suggestion
+     * @return mixed
+     */
+    public function valideElementSuggest()
+    {
+        $elementSuggestId = Input::get('elementSuggestId');
+
+        $elementSuggest = ElementSuggest::find($elementSuggestId);
+        $elementSuggest->status = 1;
+        $elementSuggest->save();
+
+        return Response::json($elementSuggest);
+    }
+
+     /**
+     * Ajax Request : refuse an element suggestion
+     * @return mixed
+     */
+    public function refuseElementSuggest()
+    {
+        $elementSuggestId = Input::get('elementSuggestId');
+
+        $elementSuggest = ElementSuggest::find($elementSuggestId);
+        $elementSuggest->status = 2;
+        $elementSuggest->save();
+
+        return Response::json($elementSuggest);
     }
 }

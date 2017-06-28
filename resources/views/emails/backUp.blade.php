c10d56c34c3af050d75d4628803900ce4b9a544b
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-06-28 20:03:00 +0200

    Liaison des url des emails avec le site en prod

--- resources/views/emails/backUp.blade.php
+++ resources/views/emails/backUp.blade.php
@@ -1,6 +1,6 @@
 <p>Bonjour,</p>
 
-<p>Voici <a href="http://annuel/checkToken/{{$token}}">le lien</a> qui vous permettra de récupérer votre compte.</p>
+<p>Voici <a href="http://club-des-critiques.gqui.eu/checkToken/{{$token}}">le lien</a> qui vous permettra de récupérer votre compte.</p>
 <p>Nous vous invitons à enregistrer un nouveau mot de passe qui remplacera l'actuel.</p>
 
 <p>Merci</p>

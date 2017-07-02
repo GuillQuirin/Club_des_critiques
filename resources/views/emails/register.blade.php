7ec970b8514492d1ff677691b9ab6c4c851c5e4b
Reconnois <GUI.QUIRIN@hotmail.fr>
2017-07-02 23:52:43 +0200

    Liens de redirection automatiques dans les emails

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,6 +1,6 @@
 <p>Bonjour,</p>
 
-<p>Voici <a href="http://club-des-critiques.gqui.eu/checkToken/{{$token}}">le lien</a> pour confirmer votre inscription.</p>
+<p>Voici <a href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/checkToken/{{$token}}">le lien</a> pour confirmer votre inscription.</p>
 <p>Nous vous invitons à enregistrer un mot de passe qui servira à vous authentifier à l'avenir.</p>
 
 <p>Merci</p>
